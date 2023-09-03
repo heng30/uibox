335e171d787cd498ff1a7f429079747eb077db4a
blueJpg <2238288979@qq.com>
2023-09-03 09:21:45 +0800

    [*] Improve find system fonts efficiently

--- uibox/src/logic/fonts.rs
+++ uibox/src/logic/fonts.rs
@@ -32,87 +32,89 @@ pub fn init(ui: &AppWindow) {
 
 fn show_all_fonts(ui: &AppWindow) {
     let ui_handle = ui.as_weak();
-    let _ = thread::spawn(move || {
-        let _ = slint::invoke_from_event_loop(move || {
-            let ui = ui_handle.unwrap();
+    let _ = thread::spawn(move || match SystemSource::new().all_fonts() {
+        Ok(fonts) => {
+            let mut map: HashMap<_, (HashSet<_>, _)> = HashMap::new();
+            for font in fonts {
+                if let Ok(font) = font.load() {
+                    let properties = font.properties();
 
-            match SystemSource::new().all_fonts() {
-                Ok(fonts) => {
-                    let mut map: HashMap<_, (HashSet<_>, _)> = HashMap::new();
-                    for font in fonts {
-                        if let Ok(font) = font.load() {
-                            let properties = font.properties();
-
-                            if font.postscript_name().is_none() {
-                                continue;
-                            }
-
-                            let pname = font
-                                .postscript_name()
-                                .unwrap()
-                                .split('-')
-                                .collect::<Vec<&str>>()
-                                .first()
-                                .unwrap()
-                                .split('_')
-                                .collect::<Vec<&str>>()
-                                .first()
-                                .unwrap()
-                                .to_string();
-
-                            match map.get_mut(&pname) {
-                                Some(item) => {
-                                    item.0.insert(properties.style.to_string());
-                                }
-                                _ => {
-                                    let mut set = HashSet::new();
-                                    set.insert(properties.style.to_string());
-                                    map.insert(
-                                        pname.clone(),
-                                        (
-                                            set,
-                                            FontItem {
-                                                postscript_name: pname.as_str().into(),
-                                                name: font.full_name().into(),
-                                                family: font.family_name().into(),
-                                                weight: properties.weight.0,
-                                                stretch: properties.stretch.0,
-                                                ..FontItem::default()
-                                            },
-                                        ),
-                                    );
-                                }
-                            }
-                        }
+                    if font.postscript_name().is_none() {
+                        continue;
                     }
 
-                    let items = VecModel::default();
-                    for mut item in map.into_values() {
-                        let styles: VecModel<SharedString> = VecModel::default();
-                        for style in item.0.iter() {
-                            styles.push(style.into());
+                    let pname = font
+                        .postscript_name()
+                        .unwrap()
+                        .split('-')
+                        .collect::<Vec<&str>>()
+                        .first()
+                        .unwrap()
+                        .split('_')
+                        .collect::<Vec<&str>>()
+                        .first()
+                        .unwrap()
+                        .to_string();
+
+                    match map.get_mut(&pname) {
+                        Some(item) => {
+                            item.0.insert(properties.style.to_string());
                         }
-                        let styles =
-                            SortModel::new(styles, |a, b| a.to_lowercase().cmp(&b.to_lowercase()));
-
-                        item.1.styles = Rc::new(styles).into();
-                        items.push(item.1);
+                        _ => {
+                            let mut set = HashSet::new();
+                            set.insert(properties.style.to_string());
+                            map.insert(
+                                pname.clone(),
+                                (
+                                    set,
+                                    (
+                                        pname,
+                                        font.full_name(),
+                                        font.family_name(),
+                                        properties.weight.0,
+                                        properties.stretch.0,
+                                    ),
+                                ),
+                            );
+                        }
                     }
-
-                    let items = SortModel::new(items, |a, b| {
-                        a.postscript_name
-                            .to_lowercase()
-                            .cmp(&b.postscript_name.to_lowercase())
-                    });
-
-                    ui.global::<Store>().set_fonts_cache(Rc::new(items).into());
-                    ui.global::<Store>()
-                        .set_fonts(ui.global::<Store>().get_fonts_cache());
-                }
-                Err(e) => {
-                    warn!("{:?}", e);
                 }
             }
-        });
+
+            let _ = slint::invoke_from_event_loop(move || {
+                let items = VecModel::default();
+                for item in map.into_values() {
+                    let styles: VecModel<SharedString> = VecModel::default();
+                    for style in item.0.iter() {
+                        styles.push(style.into());
+                    }
+                    let styles =
+                        SortModel::new(styles, |a, b| a.to_lowercase().cmp(&b.to_lowercase()));
+
+                    items.push(FontItem {
+                        postscript_name: item.1.0.into(),
+                        name: item.1.1.into(),
+                        family: item.1.2.into(),
+                        weight: item.1.3,
+                        stretch: item.1.4,
+                        styles: Rc::new(styles).into(),
+                    });
+                }
+
+                let items = SortModel::new(items, |a, b| {
+                    a.postscript_name
+                        .to_lowercase()
+                        .cmp(&b.postscript_name.to_lowercase())
+                });
+
+                let ui = ui_handle.unwrap();
+                ui.global::<Store>().set_fonts_cache(Rc::new(items).into());
+                ui.global::<Store>()
+                    .set_fonts(ui.global::<Store>().get_fonts_cache());
+            });
+        }
+        Err(e) => {
+            warn!("{:?}", e);
+        }
     });
 }
