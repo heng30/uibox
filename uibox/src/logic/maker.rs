87703b9c4cac19cdf29c9c17dfa33004310cd677
blueJpg <2238288979@qq.com>
2023-09-07 14:34:41 +0800

    [+] Feature: add color maker

--- uibox/src/logic/maker.rs
+++ uibox/src/logic/maker.rs
@@ -1,4 +1,4 @@
-use crate::slint_generatedAppWindow::{AppWindow, Logic};
+use crate::slint_generatedAppWindow::{AppWindow, ColorMakerConfig, Logic};
 // use log::debug;
 use slint::{Color, ComponentHandle};
 
@@ -27,23 +27,46 @@ pub fn init(ui: &AppWindow) {
             let current_band_index = (current_y / band_len) as usize;
             let current_band_offset = current_y % band_len;
 
-            if current_band_index > band_count {
-                return Color::from_rgb_u8(255, 255, 255);
-            }
-
             if current_band_index == band_count {
                 return Color::from_rgb_u8(255, 0, 255);
+            } else if current_band_index > band_count {
+                return Color::from_rgb_u8(255, 255, 255);
             }
 
             let offset_per = current_band_offset / band_len;
             let (start_red, start_green, start_blue) = COLOR_BAND[current_band_index];
             let (end_red, end_green, end_blue) = COLOR_BAND[current_band_index + 1];
             let (cur_red, cur_green, cur_blue) = (
-                (start_red as f32 + (end_red - start_red) as f32 * offset_per) as u8,
-                (start_green as f32 + (end_green - start_green) as f32 * offset_per) as u8,
-                (start_blue as f32 + (end_blue - start_blue) as f32 * offset_per) as u8,
+                (start_red + (end_red - start_red) * offset_per) as u8,
+                (start_green + (end_green - start_green) * offset_per) as u8,
+                (start_blue + (end_blue - start_blue) * offset_per) as u8,
             );
 
             Color::from_rgb_u8(cur_red, cur_green, cur_blue)
+        });
+
+    ui.global::<Logic>()
+        .on_current_display_color(move |height, current_y, band_color| {
+            if height < current_y || height < 0.0 || current_y < 0.0 {
+                return ColorMakerConfig::default();
+            }
+
+            let offset_per = current_y / height;
+
+            if offset_per < 0.5 {
+                let offset_per = offset_per * 2.0;
+                ColorMakerConfig {
+                    red: (255.0 - (255.0 - band_color.red() as f32) * offset_per) as i32,
+                    green: (255.0 - (255.0 - band_color.green() as f32) * offset_per) as i32,
+                    blue: (255.0 - (255.0 - band_color.blue() as f32) * offset_per) as i32,
+                }
+            } else {
+                let offset_per = 1.0 - (offset_per - 0.5) * 2.0;
+                ColorMakerConfig {
+                    red: (band_color.red() as f32 * offset_per) as i32,
+                    green: (band_color.green() as f32 * offset_per) as i32,
+                    blue: (band_color.blue() as f32 * offset_per) as i32,
+                }
+            }
         });
 }
